2e136712c14cb42bb83f1fee37ffa73fcb587ab8
Vadim Sabin <pofoq@mail.ru>
2021-01-25 12:03:02 +0300

    Lesson 3 - All done

---
+++ Lesson_3/Diagonal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_03
+{
+    public class Diagonal
+    {
+        int y;
+        int x;
+
+        int[,] arr1;
+
+        public Diagonal(int _y, int _x)
+        {
+            x = _x;
+            y = _y;
+            arr1 = new int[y, x];
+        }
+
+        public void ShowArray()
+        {
+            Console.Clear();
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    arr1[i, j] = i + j;
+                }
+            }
+
+            for (int i = 0; i < arr1.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr1.GetLength(1); j++)
+                {
+                    Console.Write(arr1[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0, j = 0; i < arr1.GetLength(0) && j < arr1.GetLength(1); i++, j++)
+            {
+                Console.SetCursorPosition(i, j + y + 2);
+                Console.WriteLine();
+                Console.WriteLine(arr1[i, j]);
+            }
+        }        
+    }
+}
---
+++ Lesson_3/Phonebook.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_03
+{
+    public class Phonebook
+    {
+        string[,] phoneArray;
+
+        public Phonebook(int countOfContacts)
+        {
+            phoneArray = new string[countOfContacts, 2];
+        }
+
+        // по задумке возвращает bool для проверки, добавился контакт или нет. 
+        // На деле не использовал, но теоретически, могло бы пригодиться на будущее. 
+        // Как минимум ошибку не выдаст
+        public bool Add(int contactNumber, string name, string phone)
+        {
+            if (contactNumber < phoneArray.GetLength(0))
+            {
+                phoneArray[contactNumber, 0] = name;
+                phoneArray[contactNumber, 1] = phone;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public void ShowAllContacts()
+        {
+            Console.Clear();
+            int maxLength = 0;
+            for (int y = 0; y < phoneArray.GetLength(0); y++)
+            {
+                Console.WriteLine(phoneArray[y, 0]);
+                if (maxLength < phoneArray[y, 0].Length)
+                    maxLength = phoneArray[y, 0].Length;
+            }
+
+            for (int y = 0; y < phoneArray.GetLength(0); y++)
+            {
+                Console.SetCursorPosition(maxLength + 3, y);
+                Console.WriteLine(phoneArray[y, 1]);
+            }
+        }
+    }
+}
