cbccad0bbf04d59e85ceb41b8dea37827c52a6ea
Vadim Sabin <pofoq@mail.ru>
2021-02-13 08:46:16 +0300

    2-Algo Base commit

---
+++ 2-Algo/MainCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MainCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
