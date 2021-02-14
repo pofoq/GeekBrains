7bb32612fba57a94029a2a8d949d507c85e10a05
Vadim Sabin <pofoq@mail.ru>
2021-02-14 12:20:24 +0300

    algo 01 done

---
+++ 2-Algo/Lesson-01/Number.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_01
+{
+    public static class Number
+    {
+        // TASK 1
+        //Напишите на C# функцию согласно блок-схеме
+        //Требуется реализовать на C# функцию согласно блок-схеме. Блок-схема описывает алгоритм проверки, простое число или нет.
+        //1.	Написать консольное приложение.
+        //2.	Алгоритм реализовать отдельно в функции согласно блок-схеме.
+        //3.	Написать проверочный код в main функции.
+        //4.	Код выложить на GitHub.
+
+        public static string IsPrimeNumber(int n)
+        {
+            int d = 0, i = 2, number = n;
+            while (i < number)
+            {
+                if (number % i == 0)
+                    d++;
+                i++;
+            }
+
+            if (d == 0)
+                return "Простое";
+            else
+                return "Не простое";
+        }
+    }
+}
---
+++ 2-Algo/Lesson-01/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Lesson_01
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                // Task 1
+                int n = GetInt();
+                Console.WriteLine(Number.IsPrimeNumber(n));
+                Console.WriteLine();
+
+                // Task 2
+                int[] arr = new int[n];
+                Random random = new Random();
+                for (int i = 0; i < n; i++)
+                {
+                    arr[i] = random.Next(1, n);
+                }
+                Console.Write($"Strange Sum: {Task2.StrangeSum(arr)}");
+                Console.WriteLine();
+                Console.WriteLine();
+
+                // Task 3
+                Stopwatch sw = new Stopwatch();
+                sw.Restart();
+                Console.WriteLine($"Фибоначчи Цикл:\t\t{Fibonachi.GetCycle(n)}");
+                sw.Stop();
+                Console.WriteLine("время выполнения: " + sw.ElapsedMilliseconds);
+                Console.WriteLine();
+                sw.Restart();
+                Console.WriteLine($"Фибоначчи Рекурсия:\t{Fibonachi.GetRecure(n)}");
+                sw.Stop();
+                Console.WriteLine("время выполнения: " + sw.ElapsedMilliseconds);
+                Console.WriteLine();
+                Console.WriteLine("For exit press Q...");
+                if (Console.ReadKey().Key == ConsoleKey.Q)
+                    break;
+                Console.Clear();
+            }
+        }
+
+        static int GetInt()
+        {
+            int n;
+            Console.Write("Введите целое число: ");
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine();
+                Console.Write("Ошибка, введите целое число: ");
+            }
+            Console.WriteLine();
+            Console.Clear();
+            return n;
+        }
+    }
+}
