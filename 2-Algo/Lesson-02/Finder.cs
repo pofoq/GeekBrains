7373bfa5d45affe730f208097167fd775c3b6a0d
Vadim Sabin <pofoq@mail.ru>
2021-02-19 18:36:00 +0300

    lesson 02 - TwoLinked List and BinarySearch

---
+++ 2-Algo/Lesson-02/Finder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_02
+{
+    public static class Finder
+    {
+        public static int BinarySearch(int[] inputArray, int searchValue)
+        {
+            int min = 0;    // O(1)
+            int max = inputArray.Length - 1;    // O(1)
+            while (min <= max)  // O(не знаю)
+            {
+                int mid = (min + max) / 2;  // O(1)
+                if (searchValue == inputArray[mid])
+                {
+                    return mid; // O(1)
+                }
+                else if (searchValue < inputArray[mid])
+                {
+                    max = mid - 1;  // O(1)
+                }
+                else
+                {
+                    min = mid + 1;  // O(1)
+                }
+            }
+            return -1;  // O(1)
+        }
+    }
+}
---
+++ 2-Algo/Lesson-02/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_02
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node NextNode { get; set; }
+        public Node PrevNode { get; set; }
+
+        public Node()
+        { }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+}
