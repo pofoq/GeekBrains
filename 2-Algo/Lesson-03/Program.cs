1357a63ecedbbd3628ed27d24df9bbf60299fedd
Vadim Sabin <pofoq@mail.ru>
2021-02-27 21:07:05 +0300

    BenchmarkDotNet

---
+++ 2-Algo/Lesson-03/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+using BenchmarkDotNet.Attributes;
+
+namespace lesson03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        }
+    }
+}
