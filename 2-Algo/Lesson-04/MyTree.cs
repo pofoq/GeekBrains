c4715eb02c4b8922329389ca1c89214919c90989
Sabin,Vadim <vadim.sabin@selgros.ru>
2021-03-11 09:01:44 +0300

    algo-04 NodeTree

---
+++ 2-Algo/Lesson-04/MyTree.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_04
+{
+    public class MyTree : ITree
+    {
+        TreeNode root;
+
+        public void AddItem(int value)
+        {
+            TreeNode tempNode;
+            TreeNode node = new TreeNode();
+            node.Value = value;
+
+            if (root is null)
+                root = node;
+            else
+            {
+                tempNode = root;
+                while (true)
+                {
+                    if (value <= tempNode.Value)
+                    {
+                        if (tempNode.LeftChild is null)
+                        {
+                            tempNode.LeftChild = node;
+                            node.Parent = tempNode;
+                            break;
+                        }
+                        tempNode = tempNode.LeftChild;
+                    }
+                    else
+                    {
+                        if (tempNode.RightChild is null)
+                        {
+                            tempNode.RightChild = node;
+                            node.Parent = tempNode;
+                            break;
+                        }
+                        tempNode = tempNode.RightChild;
+                    }
+                }
+            }
+        }
+
+        public TreeNode GetNodeByValue(int value)
+        {
+            TreeNode node = root;
+            while (node != null)
+            {
+                if (value == node.Value)
+                    break;
+                else if (value <= node.Value)
+                    node = node.LeftChild;
+                else
+                    node = node.RightChild;
+            }
+            return node;
+        }
+
+        public TreeNode GetRoot()
+        {
+            return root;
+        }
+
+        public void PrintTree()
+        {
+            Console.Clear();
+
+            NodeInfo[] nodes = TreeHelper.GetTreeInLine(this);
+
+            foreach (NodeInfo node in nodes)
+            {
+                for (int i = 0; i < node.Depth; i++)
+                {
+                    Console.Write("    ");
+                }
+                Console.Write(node.Node.Value);
+                Console.WriteLine();
+            }
+        }
+
+        public void RemoveItem(int value)
+        {
+            TreeNode toDelete = GetNodeByValue(value);
+            if (toDelete is null)
+                return;
+            TreeNode temp;
+            TreeNode middle;
+
+            if (root.Value <= value)
+            {
+                middle = toDelete.LeftChild;
+                if (middle != null)
+                {
+                    temp = toDelete.LeftChild;
+                    while (temp != null)
+                    {
+                        middle = temp;
+                        temp = temp.RightChild;
+                    }
+                    if (middle.Parent != null && toDelete.Equals(middle.Parent))
+                    {
+                        middle.Parent = toDelete.Parent;
+                        middle.RightChild = toDelete.RightChild;
+                    }
+                    if (middle.Parent != null && !toDelete.Equals(middle.Parent))
+                    {
+                        middle.Parent.RightChild = middle.LeftChild;
+                        middle.Parent = toDelete.Parent;
+                        middle.LeftChild = toDelete.LeftChild;
+                        middle.RightChild = toDelete.RightChild;
+                    }
+                }
+            }
+            else
+            {
+                middle = toDelete.RightChild;
+                if (middle != null)
+                {
+                    temp = toDelete.RightChild;
+                    while (temp != null)
+                    {
+                        middle = temp;
+                        temp = temp.LeftChild;
+                    }
+                    if (middle.Parent != null && toDelete.Equals(middle.Parent))
+                    {
+                        middle.Parent = toDelete.Parent;
+                        middle.LeftChild = toDelete.LeftChild;
+                    }
+                    if (middle.Parent != null && !toDelete.Equals(middle.Parent))
+                    {
+                        middle.Parent.LeftChild = middle.RightChild;
+                        middle.Parent = toDelete.Parent;
+                        middle.LeftChild = toDelete.LeftChild;
+                        middle.RightChild = toDelete.RightChild;
+                    }
+                }
+            }
+
+            if (toDelete.Parent != null)
+            {
+                if (toDelete.Equals(toDelete.Parent.LeftChild))
+                    toDelete.Parent.LeftChild = middle;
+                else if (toDelete.Equals(toDelete.Parent.RightChild))
+                    toDelete.Parent.RightChild = middle;
+            }
+            else if (root.Equals(toDelete))
+                root = middle;
+ 
+            toDelete.LeftChild = null;
+            toDelete.RightChild = null;
+            toDelete.Parent = null;
+        }
+    }
+}
---
+++ 2-Algo/Lesson-04/Program.cs
@@ -0,0 +1,86 @@
+﻿using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Attributes;
+using System;
+
+namespace Lesson_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 2
+            MyTree tree = new MyTree();
+            tree.AddItem(50);
+            tree.RemoveItem(50);
+            Console.WriteLine(tree.GetRoot() is null ? "NULL" : tree.GetRoot().Value.ToString());
+            Console.WriteLine($"Expected: NULL");
+            tree.AddItem(50);
+            tree.AddItem(25);
+            tree.AddItem(15);
+            tree.AddItem(10);
+            tree.AddItem(20);
+            tree.AddItem(40);
+            tree.AddItem(35);
+            tree.AddItem(45);
+            tree.AddItem(75);
+            tree.AddItem(65);
+            tree.AddItem(60);
+            tree.AddItem(70);
+            tree.AddItem(90);
+            tree.AddItem(80);
+            tree.AddItem(95);
+            TreeNode node;
+            node = tree.GetNodeByValue(75);
+            Console.WriteLine(node.Parent.Value);
+            Console.WriteLine($"Expected: 50");
+            node = tree.GetNodeByValue(65);
+            Console.WriteLine(node.RightChild.Value);
+            Console.WriteLine($"Expected: 70");
+            tree.RemoveItem(75);
+            Console.WriteLine(node.RightChild is null ? "NULL" : node.RightChild.Value.ToString());
+            Console.WriteLine($"Expected: NULL");
+            Console.WriteLine(tree.GetRoot().Value);
+            Console.WriteLine($"Expected: 50");
+            tree.RemoveItem(50);
+            Console.WriteLine(tree.GetRoot().Value);
+            Console.WriteLine($"Expected: 45");
+            tree.AddItem(37);
+            tree.AddItem(33);
+            node = tree.GetNodeByValue(37);
+            Console.WriteLine(node.Parent.Value);
+            Console.WriteLine($"Expected: 35");
+            node = tree.GetNodeByValue(33);
+            Console.WriteLine(node.Parent.Value);
+            Console.WriteLine($"Expected: 35");
+
+            Console.ReadKey();
+            tree.PrintTree();
+
+            Console.ReadKey();
+
+            // Task 1
+            TestFinder();
+            Console.ReadKey();
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+            Console.ReadKey();
+        }
+
+        static void TestFinder()
+        {
+            Finder finder = Finder.FillRandom();
+            int num = finder.arrLength / 2;
+            string toFind = finder.arrString[num];
+            Console.WriteLine($"String to find: \"{toFind}\"");
+            Console.WriteLine();
+            Console.WriteLine(finder.FindInArray(toFind));
+            Console.WriteLine($"\tExpected True");
+            Console.WriteLine(finder.FindInHashSet(toFind));
+            Console.WriteLine($"\tExpected True");
+            Console.WriteLine(finder.FindInArray("wrong"));
+            Console.WriteLine($"\tExpected False");
+            Console.WriteLine(finder.FindInHashSet("wrong"));
+            Console.WriteLine($"\tExpected False");
+            Console.WriteLine();
+        }
+    }
+}
